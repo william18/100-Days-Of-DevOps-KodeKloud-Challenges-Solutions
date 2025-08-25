397820cd7c99db121d2cccd001b53af7de9872ab
Melki Meriem <meriem.melki@insat.ucar.tn>
2025-08-25 13:21:07 +0100

    DAY 24

---
+++ Day 24: Git Create Branches/commands.sh
@@ -0,0 +1,14 @@
+# 1. Go to the repository
+cd /usr/src/kodekloudrepos/games
+
+# 2. Make sure you are on master branch
+git checkout master
+
+# 3. Pull the latest changes (optional but good practice)
+git pull origin master
+
+# 4. Create a new branch named xfusioncorp_games from master
+git checkout -b xfusioncorp_games
+
+# 5. Push the new branch to remote (if required by Nautilus setup)
+git push origin xfusioncorp_games
