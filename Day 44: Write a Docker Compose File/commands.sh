17db1464086b5f6103cb07f987d16448646dfb86
Melki Meriem <meriem.melki@insat.ucar.tn>
2025-09-14 16:58:51 +0100

    Day 44

---
+++ Day 44: Write a Docker Compose File/commands.sh
@@ -0,0 +1,8 @@
+# 1. Go to the required directory
+cd /opt/docker
+
+# 2. Create the docker-compose.yml file ( put the content here ) :
+vi docker-compose.yml
+
+# 3. Run docker-compose
+docker compose up -d
