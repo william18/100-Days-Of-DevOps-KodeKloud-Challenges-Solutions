a13acd050a094b2ebca6c45341a73771b0905d01
William <william18@users.noreply.github.com>
2025-09-17 11:38:17 +0000

    Update commands.sh
    docker compose installations update

--- Day 35: Install Docker Packages and Start Docker Service Start/commands.sh
+++ Day 35: Install Docker Packages and Start Docker Service Start/commands.sh
@@ -17,8 +17,15 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 # 5️⃣ Install Docker Compose
-DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+#DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+#sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo dnf install -y libxcrypt-compat
+
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
